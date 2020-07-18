c369d88a49b4e6c47083bc35a57d8f80c27cdfb6
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2020-07-18 14:47:33 +0100

    more generic api

--- ota_api.py
+++ ota_api.py
@@ -1,74 +1,52 @@
-import requests, semver
 from flask import Flask, jsonify, request
+import requests, semver
 
-GITHUB_USERNAME = 'kaizoku-619'
-GITHUB_REPOSITORY = 'pio-ci-example'
-
-# Define app using Flask
 app = Flask(__name__)
 
 # Root endpoint
-@app.route('/', methods=['GET'])
-def root():
-    return 'Welcome to GitHub OTA API'
+@app.route("/", methods=["GET"])
+def root_handler():
+  return "Welcome to GitHub OTA API"
 
-# Get download URL endpoint
-@app.route('/firmwares/latest', methods=['GET'])
+# Get latest firmware download URL endpoint 
+@app.route("/firmwares/latest", methods=["GET"])
+def latest_release_url_handler():
+  download_url = get_latest_release_url()
+  if download_url:
+    ret = {"download_url": download_url}
+  else:
+    # HTTP 204 No Content
+    ret = "", 204
+  return ret
+
 def get_latest_release_url():
-    device_fw_version = request.args.get('version')
-    board = request.args.get('board')
-    
-    if device_fw_version and board:
-        ret, download_url = check_board_fw_version(device_fw_version, \
-                                                   board,             \
-                                                   GITHUB_USERNAME,   \
-                                                   GITHUB_REPOSITORY)
-    else:
-        if not device_fw_version:
-            print('[ERROR] No version found')
-        if not board:
-            print('[ERROR] No board found')
-        return '', 400
-
-    if ret == True:
-        return download_url
-    else:
-        # HTTP 204 No Content
-        return '', 204
-
-# Get URL of the latest firmware release binary
-def check_board_fw_version(device_fw_version, board, github_user, github_repo):
-    ret, download_url = False, ''
-
-    response = requests.get('https://api.github.com/repos/{}/{}/releases/latest' \
-                       .format(github_user, github_repo))                        \
-                       .json()
-    server_fw_version = response['tag_name'][1:]
-    if semver.VersionInfo.isvalid(device_fw_version) and \
-       semver.VersionInfo.isvalid(server_fw_version):
-        # if device_fw_version < server_fw_version
-        if semver.compare(server_fw_version, device_fw_version) == 1:
-            assets = response['assets']
-            for asset in assets:
-                # examples: 'firmware_esp32dev.bin'
-                #           'firmware_nodemcuv2.bin'
-                if asset['name'] == 'firmware_{}.bin'.format(board):
-                    download_url = asset['browser_download_url']
-
-                    print('download_url = {}'.format(download_url))
-                    r = requests.get(download_url)
-                    print(r)
-                    if r.history:
-                        print('Request was redirected')
-                        resp = r.history[0]
-                        if r.status_code == 200:
-                            print('Final destination:')
-                            print(r.status_code, r.url)
-                            return True, r.url
-
-    return ret, download_url
+  download_url = ""
+  # 1. Get parameters from device http request
+  device_current_fw_version = request.args.get("device_current_fw_version")
+  # board_variant = request.args.get("board")
+  github_username = request.args.get("github_username")
+  github_repository = request.args.get("github_repository")
+  # 2. Build GitHub API URL
+  github_api_url = "https://api.github.com/repos/{}/{}/releases/latest".format(github_username, github_repository)
+  # 3. Send request to GitHub API and jsonify response
+  json_response = requests.get(github_api_url).json()
+  server_fw_version = json_response["tag_name"]
+  # 4. if tag_name starts with "v" remove it, ex: "v1.0.1" ==> "1.0.1"
+  if server_fw_version[0] == "v":
+    server_fw_version = json_response["tag_name"][1:]
+  # 5. Check versions semantic validity
+  if semver.VersionInfo.isvalid(device_current_fw_version) and semver.VersionInfo.isvalid(server_fw_version):
+    # 5.1. if device_current_fw_version < server_fw_version
+    if semver.compare(server_fw_version, device_current_fw_version) == 1:
+      # 5.2. extract download url from response
+      assets = json_response["assets"]
+      for asset in assets:
+        if asset["name"] == "firmware.bin":
+          download_url = asset["browser_download_url"]
+          break
+  return download_url
 
 if __name__ == "__main__":
-    # Run app on port 8080 in debug mode,
-    # host='0.0.0.0' is used to make the app discoverable on LAN
-    app.run(host='0.0.0.0', port=8080, debug=True)
+  # Run app on port 8080 in debug mode,
+  # host="0.0.0.0" is used to make the app discoverable on LAN
+  app.run(host="0.0.0.0", port=8080, debug=True)
--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 import requests
 
-FILE_URL = 'https://github.com/kaizoku-619/pio-ci-example/releases/download/v1.0.1/firmware_esp32dev.bin'
+FILE_URL = "https://github.com/kaizoku-619/pio-ci-example/releases/download/v1.0.1/firmware_esp32dev.bin"
 
 def main():
-    response = requests.get(FILE_URL)
-    print(response)
-    if response.history:
-        print('Request was redirected')
-        resp = response.history[0]
-        if response.status_code == 200:
-            print('Final destination:')
-            print(response.status_code, response.url)
-    else:
-        print('Request was not redirected')
+  response = requests.get(FILE_URL)
+  print(response)
+  if response.history:
+    print("Request was redirected")
+    if response.status_code == 200:
+      print("Final destination:")
+      print(response.status_code, response.url)
+  else:
+    print("Request was not redirected")
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+  main()
